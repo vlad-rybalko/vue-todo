df1beebacb162ab07de225a7adf179b0b72c0eb0
Vadislav_Rybalko <vlad_rybalko@mail.ru>
2020-08-07 16:53:49 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo application</h1>
+    <AddTodo />
+    <hr>
+    <TodoList 
+      v-bind:todos="todos"
+      v-on:remove-todo="removeTodoItem"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
 export default {
   name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Купить хлеб', completed: false},
+        {id: 2, title: 'Купить масло', completed: false},
+        {id: 3, title: 'Купить воду', completed: false}
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    TodoList, AddTodo
+  },
+  methods: {
+    removeTodoItem(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    }
   }
 }
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <input type="text">
+        <button type="submit">Создать</button>
+    </form>
+</template>
+
+<script>
+export default {
+    methods: {
+        onSubmit() {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input {
+        width: 400px;
+    }
+</style>
